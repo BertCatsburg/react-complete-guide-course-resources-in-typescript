391fada358bdf2e6d0cd7deb4db912ca5aad5283
Bert Catsburg <bert@catsburg.com>
2023-12-24 14:28:31 +0100

    End of lesson 295

--- section-015-http-requests/src/components/AvailablePlaces.tsx
+++ section-015-http-requests/src/components/AvailablePlaces.tsx
@@ -1,16 +1,30 @@
 import {Places} from './index'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {PlacesDataInterface} from "../types/types";
 
 interface AvailablePlacesInterface {
   onSelectPlace: (selectedPlace: PlacesDataInterface) => void
 }
 
-export const   AvailablePlaces = ({onSelectPlace}: AvailablePlacesInterface) => {
+export const AvailablePlaces = ({onSelectPlace}: AvailablePlacesInterface) => {
+
+  const [availablePlases, setAvailablePlaces] = useState<PlacesDataInterface[]>([])
+
+  useEffect(() => {
+  fetch('http://localhost:3000/places')
+    .then((response) => {
+      return response.json()
+    })
+    .then((responseJSON) => {
+      setAvailablePlaces(responseJSON.places)
+    })
+  },[])
+
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlases}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
--- section-015-http-requests/src/components/Places.tsx
+++ section-015-http-requests/src/components/Places.tsx
@@ -21,7 +21,7 @@ export const  Places = ({ title, places, fallbackText, onSelectPlace }: PlacesIn
           {places.map((place) => (
             <li key={place.id} className="place-item">
               <button onClick={() => onSelectPlace(place)}>
-                <img src={place.image.src} alt={place.image.alt} />
+                <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
                 <h3>{place.title}</h3>
               </button>
             </li>
