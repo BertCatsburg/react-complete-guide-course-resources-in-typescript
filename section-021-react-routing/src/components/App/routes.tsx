74fb7e48d5743b223fb17c2d71fab50e419041c1
Bert Catsburg <bert@catsburg.com>
2024-01-01 12:26:48 +0100

    Cosmetics

--- section-021-react-routing/src/components/App/routes.tsx
+++ section-021-react-routing/src/components/App/routes.tsx
@@ -2,13 +2,25 @@ import {createBrowserRouter} from 'react-router-dom'
 import React from 'react'
 import {HomePage, ProductsPage} from '../../pages'
 
+// *** Older way of defining routes
+// import {createRoutesFromElements, Route} from "react-router-dom";
+//
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />}/>
+//     <Route path="/products" element={<ProductsPage />}/>
+//   </Route>
+// )
+//
+// export const router = createBrowserRouter(routeDefinitions)
+
 export const router = createBrowserRouter([
   {
     path: '/', // Main Page on the domain
-    element: <HomePage />
+    element: <HomePage/>
   },
   {
     path: '/products',
-    element: <ProductsPage />
+    element: <ProductsPage/>
   }
 ])
